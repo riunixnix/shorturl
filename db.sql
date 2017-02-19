d0b7f5ae69730d1cf1173eefe68beefcb6bc1be7
Darwin Kesuma <darwinkesuma@gmail.com>
2017-02-20 03:27:07 +0800

    update

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS shorturl;
 
-CREATE TABLE IF NOT EXISTS `urls` (
+CREATE TABLE IF NOT EXISTS `urls3` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `full_url` varchar(1000) NOT NULL,
+  `full_url` varchar(600) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `urls_full_url_IDX` (`full_url`)
 ) ENGINE=InnoDB;
